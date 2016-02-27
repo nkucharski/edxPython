488825a5a4793273129c76599f21197feddc20cb
nkucharski <batman105@gmail.com>
2016-02-27 15:02:13 -0800

    Added files via upload

--- edxAlphabet.py
+++ edxAlphabet.py
@@ -1,26 +1,30 @@
 #s This is a program which prints the largest substring of a string "s", in which the letters occur in alphabetical order.
 
 def sSub(s):
-	currentlength = 0
+	currentlength = 1
 	currentstart = 0
 	beststart = 0
-	bestlength = 0
+	bestlength = 1
 	for i in range(len(s) - 1):
 		if s[i] < s[i+1]:
 			currentlength += 1
 			print('currentlength = ' + str(currentlength))
 			print('i = ' + str(i))
-			print('s[i] = ' + s[i] + ' s[i+1] = ' + s[i+1]) 
-		elif s[i] > s[i+1] or s[i+1]==None:
-			print('WARNING: s[i] > s[i+1]')
-			print('i = ' + i)
+			print('s[i] = ' + s[i] + ' s[i+1] = ' + s[i+1])
 			if currentlength > bestlength:
 				bestlength = currentlength
-				beststart = currentstart
+				beststart = currentstart + 1
+		elif s[i] > s[i+1]:
+			print('WARNING: s[i] > s[i+1]')
+			print('i = ' + str(i))
+			if currentlength > bestlength:
+				bestlength = currentlength
+				beststart = currentstart + 1
 				print('bestlength = ' + str(bestlength))
 				print('beststart = ' + str(beststart))
 			currentstart = i
-			currentlength = 0
+			currentlength = 1
+	print('beststart = ' + str(beststart) + 'bestlength = ' + str(bestlength))
 	return s[beststart : beststart+bestlength]
 
 			 	
